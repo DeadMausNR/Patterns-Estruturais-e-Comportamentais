5c7719bfd5145e3e45bc08d1dd4ea75e20270a93
Nathan Lima <nathanlima123@gmail.com>
2020-06-07 23:59:35 -0300

    Entrega de exercicio

---
+++ src/exercicio_6.1/MensagemDoSabado.java
@@ -0,0 +1,9 @@
+package exercicio_6_1;
+
+public class MensagemDoSabado implements MensagemDoDia {
+
+	@Override
+	public String mensagem() {
+		return "Hoje é sábado!";
+	}
+}
---
+++ src/exercicio_7.1/TransformarDuplicar.java
@@ -0,0 +1,9 @@
+package exercicio_7_1;
+
+public class TransformarDuplicar extends TransformaStringTemplate {
+
+	@Override
+	public void alterarString(String s) {
+		System.out.println("Convertido: " + s + " " + s);
+	}
+}
